fdff20e330291e1f3575dfb673f853cc412253b9
Patryk Damec <Patryk_Damec@epam.com>
2020-06-08 23:52:34 +0200

    Add task 2

--- tasks/modules/Module 2/task_1.py
+++ tasks/modules/Module 2/task_1.py
@@ -1,3 +1,4 @@
+
 def display_inventory(inventory):
     print('Inventory:')
     item_total = 0
@@ -12,7 +13,34 @@ def display_inventory(inventory):
         print('CAUTION: Your equipment is very heavy, you\'re moving slower than usual!')
 
 
+def add_to_inventory(inventory, added_items):
+    excluded_items = ['chewed gum', 'rubbish', 'used tissue']
+    skipped_items = {}
+    num_of_added_items = 0
+
+    # Update inventory or exclude items
+    for added_item in added_items:
+        if added_item not in excluded_items:
+            inventory[added_item] = inventory.get(added_item, 0) + 1
+            num_of_added_items += 1
+        else:
+            skipped_items[added_item] = skipped_items.get(added_item, 0) + 1
+
+    # Print results
+    print(f'Added {num_of_added_items} items to the inventory')
+    print('Skipped:')
+    for item_name, item_num in skipped_items.items():
+        print(item_num, item_name)
+
+    return inventory
+
+
 if __name__ == '__main__':
     stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+    inv = {'gold coin': 42, 'rope': 1}
+    dragonLoot = [
+        'gold coin', 'chewed gum', 'dagger', 'gold coin', 'gold coin', 'ruby',
+        'rubbish', 'chewed gum', 'used tissue']
 
-    display_inventory(stuff)
+    inv = add_to_inventory(inv, dragonLoot)
+    display_inventory(inv)
